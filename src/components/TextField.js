835ac0e3d4f7af0ad08b666770de120d777190a9
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-24 11:41:12 -0700

    feat: add TextField + submit button to AddTodo

---
+++ src/components/TextField.js
@@ -0,0 +1,16 @@
+
+const TextField = ({ label, inputProps, onChange, value }) => {
+  return (
+    <div className='flex flex-col'>
+      <label className='mb-2 text-lg uppercase font-semibold text-gray-800'>{label}</label>
+      <input
+        className='bg-gray-200 mb-3 py-2 px-3 border-2 outline-none'
+        {...inputProps}
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  )
+}
+
+export default TextField
--- src/features/todos/AddTodo.js
+++ src/features/todos/AddTodo.js
@@ -1,6 +1,37 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import { Button, TextField } from "../../components"
+
 const AddTodo = () => {
+  const navigate = useNavigate()
+  const [values, setValues] = useState({
+    title: '',
+    details: ''
+  })
+
+  const handleAddTodo = () => {
+    setValues({ title: '', details: ''})
+    console.log(values);
+    navigate('/')
+  }
+
+
   return (
-    <div>AddTodo</div>
+    <div className="mt-10 max-w-xl mx-auto">
+      <TextField
+        label="Todo"
+        value={values.title}
+        onChange={(e) => setValues({ ...values, title: e.target.value})}
+        inputProps={{ type: 'text', placeholder: 'Todo Item Title'}}
+      />
+      <TextField
+        label="Details"
+        value={values.details}
+        onChange={(e) => setValues({ ...values, details: e.target.value})}
+        inputProps={{ type: 'text', placeholder: 'Todo item details...'}}
+      />
+      <Button onClick={handleAddTodo}>Submit</Button>
+    </div>
   )
 }
 export default AddTodo
