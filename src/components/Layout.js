f27da2c5bf424d34c3e8a8cca6a57daf58eccd09
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-25 12:27:18 -0700

    styles: update UI

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,12 @@ import Header from "./Header"
 
 const Layout = () => {
   return (
-    <>
+    <div className="w-screen h-screen bg-slate-100">
       <Header />
       <main className="container mx-auto px-3 md:px-5 max-w-5xl pt-10 md:pt-12 ">
         <Outlet />
       </main>
-    </>
+    </div>
   )
 }
 export default Layout
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -4,7 +4,7 @@ const TextField = ({ label, inputProps, onChange, value }) => {
     <div className='flex flex-col'>
       <label className='mb-2 text-lg uppercase font-semibold text-gray-800'>{label}</label>
       <input
-        className='bg-gray-200 mb-3 py-2 px-3 border-2 outline-none'
+        className='bg-gray-200 mb-3 py-2 px-3 border-2 outline-none rounded'
         {...inputProps}
         onChange={onChange}
         value={value}
