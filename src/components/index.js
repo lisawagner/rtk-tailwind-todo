7f782e8f96d36e5e7e4bfd5c9f765f840268e3a9
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-25 12:26:53 -0700

    refactor: add form container & styles

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Header from "./Header";
 import Layout from "./Layout";
 import NotFound from './NotFound'
 import TextField from "./TextField";
+import FormContainer from "./FormContainer";
 
-export { Button, Header, Layout, TextField, NotFound }
+export { Button, Header, Layout, TextField, NotFound, FormContainer }
